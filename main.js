41bb8884effd8fadfadbbaa4845110737ffa74c0
Quinn Rohlf <qr@qrohlf.com>
2014-11-06 09:11:57 -0800

    Command history

--- main.js
+++ main.js
@@ -28,27 +28,64 @@ var handleInput = main;
 $(document).ready(function() {
   //Get a handle on the prompt element
   var prompt = $('#prompt');
+  var history = [];
+  var history_index = 0;
+  var temp_command;
 
   //Attach a submit handler to the form
   $('form').on('submit', function(event) {
+    history.push(prompt.val());
     handleInput(prompt.val());
     prompt.val('');
     event.preventDefault();
+    history_index = history.length;
     return false; //prevents the form submission event from being passed to the next handler
   });
 
-  for(i=0; i<10; i++) {
-    println('foobar '+i);
-  }
+  $('form').on('keyup', function(event) {
+    var update = false;
+    switch(event.keyCode) {
+      case 38: //up arrow
+        if (history_index == history.length) {
+          temp_command = prompt.val();
+        }
+        history_index -= 1;
+        update = true;
+        break;
+      case 40: //down arrow
+        history_index += 1;
+        update = true;
+        break;
+    }
+    if (update) {
+        history_index = Math.max(history_index, 0);
+        history_index = Math.min(history_index, history.length);
+        if (history_index == history.length) {
+          prompt.val(temp_command);
+        } else {
+          prompt.val(history[history_index]);
+        }
+    }
+  });
+
+  welcome();
 
 });
 
 function println(item) {
-  var logline = $('<div class="logline"></div>').append(item);
-  var append = $('.log').append(logline);
-  if (logline[0].firstElementChild) {
-    return $(logline[0].firstElementChild);
+  var logline = $('<div class="logline"></div>').append(item)[0];
+  $('.log').append(logline);
+  logline.scrollIntoView();
+  if (logline.firstElementChild) {
+    return $(logline.firstElementChild);
   } else {
-    return $(logline[0]);
+    return $(logline);
   }
+}
+
+function welcome() {
+  println('Hi! Welcome to jsconsole.');
+  println('This is a very simple prompt that you can use to play around with JavaScript.');
+  println('To get started, check out the Readme.md file.');
+  println('If you already know a little JavaScript, you can also check out the main.js file.');
 }
