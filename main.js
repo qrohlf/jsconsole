0c02c00c8fb9298197b602bc9afb571bda58caf1
Quinn Rohlf <qr@qrohlf.com>
2014-11-06 09:29:47 -0800

    snapchat! :ghost:

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ function main(prompt_value) {
   var words = prompt_value.split(' ');
   if (words[0] == 'img') {
     var image_url = words[1];
-    println($("<img src='"+image_url+"'/>"));
+    var timeout = parseInt(words[2]);
+    if (!timeout) {
+      timeout = 5;
+    }
+    var snap = println($("<img src='"+image_url+"'/>"));
+    var timer = setInterval(function() {
+      timeout -=1;
+      println(timeout+' seconds remaining');
+      if (timeout === 0) {
+        snap.remove();
+        clearInterval(timer);
+      }
+    }, 1000);
   } else {
     println(prompt_value);
   }
