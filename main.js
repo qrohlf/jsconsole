d746d6787803e0479ab2581697e3cf12522af02e
Quinn Rohlf <qr@qrohlf.com>
2014-11-09 08:24:36 -0800

    Add some comments

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 //Declare a main function
 function main(prompt_value) {
+  // Split the string the user entered into its component words
   var words = prompt_value.split(' ');
+
   if (words[0] == 'img') {
+    // If the first word is 'img', then assume the second word is
+    // a link to an image and display it
     var image_url = words[1];
     println($("<img src='"+image_url+"'/>"));
   } else {
+    // If the first word is not 'img', just print the whole string back out
     println(prompt_value);
   }
 }
